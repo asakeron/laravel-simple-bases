b7287fedcd66abb776fbccb652764ccc483dbf8a
André <abs+github@emancipe.se>
2023-07-27 17:38:10 +0000

    drop support for classes generation

--- src/Providers/LaravelSimpleBaseProvider.php
+++ src/Providers/LaravelSimpleBaseProvider.php
@@ -1,36 +1,14 @@
 <?php
 
-
 namespace LaravelSimpleBases\Providers;
 
-
 use Illuminate\Support\ServiceProvider;
-use LaravelSimpleBases\Commands\FromToDataConfig;
-use LaravelSimpleBases\Commands\GenerateEndpointClass;
-use LaravelSimpleBases\Commands\GeneratePermissionClass;
-use LaravelSimpleBases\Commands\GeneratePermissionHandle;
-use LaravelSimpleBases\Commands\KrloveEloquentModelGeneratorConfig;
-use LaravelSimpleBases\Commands\ModelWithFileConfig;
 
 class LaravelSimpleBaseProvider extends ServiceProvider
 {
-
-    public function boot()
+    public function boot(): void
     {
         $this->loadMigrationsFrom(__DIR__ . '/../Database/migrations');
         $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
     }
-
-    public function register()
-    {
-        $this->commands([
-            KrloveEloquentModelGeneratorConfig::class,
-            FromToDataConfig::class,
-            ModelWithFileConfig::class,
-            GenerateEndpointClass::class,
-            GeneratePermissionClass::class,
-            GeneratePermissionHandle::class
-        ]);
-    }
-
 }
